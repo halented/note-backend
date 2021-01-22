8c7efb574611795187d95f16908ef5f2136d47b4
Hal Dunn <heyhal19@gmail.com>
2021-01-22 09:05:52 -0800

    refacoring for model file && adds mongoose-seeder

--- models/note.model.js
+++ models/note.model.js
@@ -12,7 +12,5 @@ const noteSchema = new Schema({
     timestamps: true
 })
 
-// I am not 100% but i think this is where it gets migrated over to mongo on atlas
-const Note = mongoose.model('Note', noteSchema)
 
-module.exports = Note
+module.exports = mongoose.model('Note', noteSchema)
--- seeds.js
+++ seeds.js
@@ -1,12 +1,76 @@
-// run drop commands here
+const seeder = require("mongoose-seed")
+const User = require('./models/user.model')
+const Note = require('./models/note.model')
 
-const users = [{ username: "imi", email: "imi@ymail.com" }, { username: "Wisteria", email: "alabastor@wimpii.com" }, { username: "elificiona", email: "rampantmarxist@gmail.com" }]
+// load in atlas database key
+require('dotenv').config()
+const source = process.env.ATLAS_CONNECTION
+// this used to be the second arg, but mongoose seed uses a callback as a second arg...not sure where the twain shall meet
+// {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// },
 
-// grab a user while saving those to db
-let user
+seeder.connect(source,
+    function () {
+
+        // Load Mongoose models
+        seeder.loadModels([
+            User,
+            Note
+        ])
+
+        // Clear specified collections
+        seeder.clearModels(['Note', 'User'], function () {
+
+            // Callback to populate DB once collections have been cleared
+            seeder.populateModels(data, function () {
+                seeder.disconnect();
+            })
+
+        })
+    }
+)
+
+var data = [
+    {
+        'model': 'User',
+        'documents': [
+            { username: "imi", email: "imi@ymail.com", age: 100 },
+            { username: "Wisteria", email: "alabastor@wimpii.com", age: 100 },
+            { username: "elificiona", email: "rampantmarxist@gmail.com", age: 100 }
+        ]
+    }
+]
 
 
-const notes = [{title: "Googling for Grace", content: "this doesn't happen except in books by moticational speakers", author: user._id}]
+
+// // drop everything to reset the DB
+// User.remove({}, () => console.log("removed users"))
+// Note.remove({}, () => console.log("removed notes"))
+
+
+// const users = [{ username: "imi", email: "imi@ymail.com", age: 100 }, { username: "Wisteria", email: "alabastor@wimpii.com", age: 100 }, { username: "elificiona", email: "rampantmarxist@gmail.com", age: 100 }]
+
+// // grab a user while saving those to db
+// let user
+
+// for (let i = 0; i < users.length; i++) {
+//     if (i === users.length - 1) {
+//         let newUser = new User(users[i])
+//         newUser.save()
+//             .then(resp => user = resp)
+//         console.log("inside loop");
+//     }
+//     let newUser = new User(users[i])
+//     newUser.save()
+// }
+
+// console.log("lol did that work")
+// console.log(user)
+
+// const notes = [{ title: "Googling for Grace", content: "this doesn't happen except in books by moticational speakers", author: user._id }]
 
 // don't forget to manually shove the noteId into the user's note array and save it at the end of iteration
 
